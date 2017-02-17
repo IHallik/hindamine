a918fa688a091aa3d87109fde0c0eadf89861853
IHallik <innar.hallik@gmail.com>
2017-02-17 11:13:27 +0200

    testing connection to the DB using variables

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-echo "this is a test of PHP running on localhost\n\r";
-$my_env_var = getenv('MY_VAR');
-echo $my_env_var;
-?>
+$link = new mysqli(getenv("DB_URL"),getenv("DB_USER"), getenv("DB_PASS"), "heroku_9f3983ebc41e915");
 
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+?>
