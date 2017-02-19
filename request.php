72df77fdcbdc56a0b9e039241ad79aefb906385b
IHallik <innar.hallik@gmail.com>
2017-02-19 18:00:29 +0200

    JS refctoring and cahnged view in use

--- request.php
+++ request.php
@@ -8,7 +8,7 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 // requsting data from database
-$res = $mysqli->query("SELECT * FROM `test_view`");
+$res = $mysqli->query("SELECT * FROM `message_view`");
 // closing database connection
 mysqli_close($mysqli);
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,22 @@
+function getMesages(){
+  var xmlhttp = new XMLHttpRequest();
+  var request_url = "request.php";
 
-var xmlhttp = new XMLHttpRequest();
-var url = "request.php";
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var myArr = JSON.parse(this.responseText);
+          fillDataList(myArr);
+      }
+  };
+  xmlhttp.open("GET", request_url, true);
+  xmlhttp.send();
 
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        myFunction(myArr);
-    }
-};
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-function myFunction(arr) {
-    var out = "";
-    var i;
-    for(i = 0; i < arr.length; i++) {
-        out += '<li>' + arr[i]["message"] + '</li>';
-    }
-    document.getElementById("data").innerHTML = out;
+  function fillDataList(arr) {
+      var out = "";
+      var i;
+      for(i = 0; i < arr.length; i++) {
+          out += '<li>' + arr[i]["message"] + '-' + arr[i]["email_address"] +  '</li> ';
+      }
+      document.getElementById("data").innerHTML = out;
+  }
 }
