3fb50a3c9a9facd09de167c7e67f4a6f218c5d55
IHallik <innar.hallik@gmail.com>
2017-02-20 17:42:48 +0200

    failed to add makejson.php to the commit

---
+++ makejson.php
@@ -0,0 +1,21 @@
+<?php
+$mysqli = new mysqli(getenv("DB_URL"),getenv("DB_USER"), getenv("DB_PASS"), "heroku_9f3983ebc41e915");
+if ($mysqli->connect_errno) {
+    $messages->message = "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    $messages->email_address = "innar.hallik@gmail.com";
+    mysqli_close($mysqli);
+} else {
+  // creating JSON file
+  $res = $mysqli->query("SELECT * FROM `message_view`");
+  // fetchin all the data
+  $messages = $res->fetch_all(MYSQLI_ASSOC);
+  // closing database connection
+  mysqli_close($mysqli);
+}
+// closing DB connection
+
+// creating JSON file
+$fp = fopen('messages.json', 'w');
+fwrite($fp, json_encode($messages));
+fclose($fp);
+?>
