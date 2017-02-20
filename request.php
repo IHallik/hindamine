c66ef6a3eee101fd96748cb68a13953a255fd692
IHallik <innar.hallik@gmail.com>
2017-02-20 17:41:54 +0200

    creating JSON file of all messages after adding new message

--- request.php
+++ request.php
@@ -5,16 +5,16 @@ header('Content-type:application/json;charset=utf-8');
 // connecting to database
 $mysqli = new mysqli(getenv("DB_URL"),getenv("DB_USER"), getenv("DB_PASS"), "heroku_9f3983ebc41e915");
 if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  $messages->message = "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  $messages->email_address = "innar.hallik@gmail.com";
+} else {
+  // requsting data from database
+  $res = $mysqli->query("SELECT * FROM `message_view`");
+  // handeling data
+  $messages = $res->fetch_all(MYSQLI_ASSOC);
 }
-// requsting data from database
-$res = $mysqli->query("SELECT * FROM `message_view`");
 // closing database connection
 mysqli_close($mysqli);
-
-// handeling data
-$resultArray = $res->fetch_all(MYSQLI_ASSOC);
-
 // sending json
-echo json_encode($resultArray);
+echo json_encode($messages);
 ?>
--- send.php
+++ send.php
@@ -6,50 +6,53 @@ $response = json_decode($str_json, true); // decoding received JSON to array
 $mysqli = new mysqli(getenv("DB_URL"),getenv("DB_USER"), getenv("DB_PASS"), "heroku_9f3983ebc41e915");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-// creating a prepared statement, sql injection safeguard
-$stmt = $mysqli->prepare("SELECT idnew_emails FROM new_emails where email_address=(?);");
-// filling statement
-$stmt->bind_param("s",$response["email"]);
-// execute query
-$stmt->execute();
-$stmt->store_result();
-
-// Get the number of rows
-$num_of_rows = $stmt->num_rows;
-$id = 0;
-if ($num_of_rows) {
-  // we have our id
-  $stmt->bind_result($id);
-  $stmt->fetch();
-  $stmt->close();
 } else {
-  $stmt->close();
-  // adding new email_address
   // creating a prepared statement, sql injection safeguard
-  $stmt = $mysqli->prepare("INSERT INTO `new_emails` (`email_address`) VALUES (?)");
-
+  $stmt = $mysqli->prepare("SELECT idnew_emails FROM new_emails where email_address=(?);");
   // filling statement
   $stmt->bind_param("s",$response["email"]);
+  // execute query
+  $stmt->execute();
+  $stmt->store_result();
+
+  // Get the number of rows
+  $num_of_rows = $stmt->num_rows;
+  $id = 0;
+  if ($num_of_rows) {
+    // we have our id
+    $stmt->bind_result($id);
+    $stmt->fetch();
+    $stmt->close();
+  } else {
+    $stmt->close();
+    // adding new email_address
+    // creating a prepared statement, sql injection safeguard
+    $stmt = $mysqli->prepare("INSERT INTO `new_emails` (`email_address`) VALUES (?)");
+
+    // filling statement
+    $stmt->bind_param("s",$response["email"]);
+
+    // execute query
+    $stmt->execute();
+    $id = mysqli_insert_id($mysqli);
+
+    // close statement
+    $stmt->close();
+  }
+  // Bind the result to variables
+  $stmt = $mysqli->prepare("INSERT INTO new_table (message, poster_email) VALUES (?,?)");
+
+  // filling statement
+  $stmt->bind_param("si",$response["message"],$id);
 
   // execute query
   $stmt->execute();
-  $id = mysqli_insert_id($mysqli);
-
   // close statement
   $stmt->close();
 }
-// Bind the result to variables
-$stmt = $mysqli->prepare("INSERT INTO new_table (message, poster_email) VALUES (?,?)");
-
-// filling statement
-$stmt->bind_param("si",$response["message"],$id);
-
-// execute query
-$stmt->execute();
-
-// close statement
-$stmt->close();
-
+// closing database connection
 mysqli_close($mysqli);
+
+// make new JSON
+include "makejson.php";
 ?>
