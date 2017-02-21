568a4d7e26913f2a8ab3ad6227071af14fa4bfdc
IHallik <innar.hallik@gmail.com>
2017-02-21 02:33:53 +0200

    adding message with DB side stored procedure

--- scripts/script.js
+++ scripts/script.js
@@ -20,27 +20,6 @@ function getMessages(timestamp){
           }
       }
   );
-  /*
-  var xmlhttp = new XMLHttpRequest();
-  var request_url = "messages.json";
-
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          var myArr = JSON.parse(this.responseText);
-          fillDataList(myArr);
-      }
-  };
-  xmlhttp.open("GET", request_url, true);
-  xmlhttp.send();
-
-  function fillDataList(arr) {
-      var out = "";
-      var i;
-      for(i = 0; i < arr.length; i++) {
-          out += '<li>' + arr[i].message + '-' + arr[i].email_address +  '</li> ';
-      }
-      document.getElementById("data").innerHTML = out;
-  }*/
 }
 var xmlhttp;
 function sendMessage() {
--- send.php
+++ send.php
@@ -7,44 +7,9 @@ $mysqli = new mysqli(getenv("DB_URL"),getenv("DB_USER"), getenv("DB_PASS"), "her
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 } else {
-  // creating a prepared statement, sql injection safeguard
-  $stmt = $mysqli->prepare("SELECT idnew_emails FROM new_emails where email_address=(?);");
+  $stmt = $mysqli->prepare("call spAddMessage(?,?);");
   // filling statement
-  $stmt->bind_param("s",$response["email"]);
-  // execute query
-  $stmt->execute();
-  $stmt->store_result();
-
-  // Get the number of rows
-  $num_of_rows = $stmt->num_rows;
-  $id = 0;
-  if ($num_of_rows) {
-    // we have our id
-    $stmt->bind_result($id);
-    $stmt->fetch();
-    $stmt->close();
-  } else {
-    $stmt->close();
-    // adding new email_address
-    // creating a prepared statement, sql injection safeguard
-    $stmt = $mysqli->prepare("INSERT INTO `new_emails` (`email_address`) VALUES (?)");
-
-    // filling statement
-    $stmt->bind_param("s",$response["email"]);
-
-    // execute query
-    $stmt->execute();
-    $id = mysqli_insert_id($mysqli);
-
-    // close statement
-    $stmt->close();
-  }
-  // Bind the result to variables
-  $stmt = $mysqli->prepare("INSERT INTO new_table (message, poster_email) VALUES (?,?)");
-
-  // filling statement
-  $stmt->bind_param("si",$response["message"],$id);
-
+  $stmt->bind_param("ss",$response["email"],$response["message"]);
   // execute query
   $stmt->execute();
   // close statement
