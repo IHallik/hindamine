531de156679ad7607dbbd38b168d7a01bfc50fe2
IHallik <innar.hallik@gmail.com>
2017-03-02 22:29:22 +0200

    creating inital form

--- scripts/script.js
+++ scripts/script.js
@@ -112,8 +112,22 @@ function initMap(locationId) {
       }
   );
 }
+function initForm(){
+  var date = new Date();
+
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
+
+  if (month < 10) month = "0" + month;
+  if (day < 10) day = "0" + day;
+
+  var today = year + "-" + month + "-" + day;
+  document.getElementById('form_date').value = today;
+}
 // initialize jQuery
 $(function() {
     getMessages();
     getSpamer();
+    initForm();
 });
